c19a8821cb2b778ebdacf40a084f9d45526c83d3
Alejandro Kaminetzky <akaminetzkyp@gmail.com>
2017-09-14 11:36:42 -0300

    Finish donation and crown logger

--- logger.py
+++ logger.py
@@ -1,12 +1,87 @@
 from api_wrapper import Wrapper
+import os
+import datetime
+import csv
 
 
 class Logger:
     def __init__(self, clan_tag):
         self.wrapper = Wrapper(clan_tag)
 
-    def log_donations(self):
-        pass
+    @staticmethod
+    def log_data(path, new_data, log_total):
 
-    def log_crowns(self):
-        pass
+        if log_total:
+            total = sum([player[1] for player in new_data])
+
+        if os.path.exists(path):
+            with open(path, 'r') as file:
+                reader = csv.reader(file)
+                if log_total:
+                    dayweek_row, datetime_row, total_row, *data = list(reader)
+                else:
+                    dayweek_row, datetime_row, *data = list(reader)
+
+            dayweek_row.append(datetime.datetime.now().strftime(
+                "%A %W"))
+            datetime_row.append(datetime.datetime.now().strftime(
+                "%Y-%m-%d %H:%M:%S"))
+
+            if log_total:
+                total_row.append(total)
+
+            for new_player in new_data:
+                if new_player[0] not in [player[0] for player in
+                                         data]:
+                    new_row = ([new_player[0]] + [''] * (len(datetime_row) - 2)
+                               + [new_player[1]])
+                    data.append(new_row)
+                else:
+                    for player in data:
+                        if new_player[0] == player[0]:
+                            player.append(new_player[1])
+
+            for player in data:
+                if player[0] not in [new_player[0] for new_player in
+                                     new_data]:
+                    player.append('')
+
+            data.sort()
+
+            with open(path, 'w') as file:
+                writer = csv.writer(file)
+                writer.writerow(dayweek_row)
+                writer.writerow(datetime_row)
+                if log_total:
+                    writer.writerow(total_row)
+                writer.writerows(data)
+
+        else:
+            with open(path, 'w') as file:
+                new_data.sort()
+                writer = csv.writer(file)
+                writer.writerow(['Day / Week', datetime.datetime.now().strftime(
+                    "%A %W")])
+                writer.writerow(['Datetime', datetime.datetime.now().strftime(
+                    "%Y-%m-%d %H:%M:%S")])
+                if log_total:
+                    writer.writerow(['Total', total])
+                writer.writerows(new_data)
+
+    def log_donations(self, path='donations.csv'):
+        data = self.wrapper.get_json()
+        new_donations = [[member['name'], member['donations']] for member in
+                         data['members']]
+        self.log_data(path, new_donations, True)
+
+    def log_crowns(self, path='crowns.csv'):
+        data = self.wrapper.get_json()
+        new_crowns = [(member['name'], member['clanChestCrowns']) for member in
+                      data['members']]
+        self.log_data(path, new_crowns, True)
+
+
+if __name__ == '__main__':
+    logger = Logger('2UQ2JJC')
+    logger.log_donations()
+    logger.log_crowns()
